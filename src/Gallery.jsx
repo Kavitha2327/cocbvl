6c39c9ca142e9d08116316db7d7a7385c861393c
Kavitha2327 <kavitharanim@adityauniversity.in>
2025-12-31 11:51:07 +0530

    responsive

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -4,12 +4,21 @@ export default function Gallery() {
   const images = [
     "/images/m/img1.jpeg",
     "/images/m/img2.jpeg",
-    "/images/m/img3.jpeg",
+    // "/images/m/img3.jpeg",
     "/images/m/img4.jpeg",
     "/images/m/img5.jpeg",
     "/images/m/img6.jpeg",
-    "/images/m/img7.jpeg",
+    // "/images/m/img7.jpeg",
     "/images/m/img8.jpeg",
+    "/images/m/img9.jpeg",
+    "/images/m/img10.jpeg",
+    "/images/m/img11.jpeg",
+    "/images/m/img12.jpeg",
+    "/images/m/img13.jpeg",
+    "/images/m/img14.jpeg",
+    "/images/m/img15.jpeg",
+    "/images/m/img16.jpeg",
+    "/images/m/img17.jpeg",
   ];
 
   return (
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
   return (
     <>
       {/* Banner Section */}
       <div className="banner-wrapper">
         <img
           src="/images/banner1.jpg"
-          alt="Church of Christ Biccaavolu Banner"
+          alt="Church of Christ Biccavolu Banner"
           className="banner-img"
         />
       </div>
@@ -29,18 +30,45 @@ export default function Header() {
           </div>
 
           {/* Navigation */}
-          <nav className="nav">
+          {/* <nav className="nav">
             <a href="#about">About Us</a>
             <a href="#events">Events</a>
             <a href="#prayer">Prayer Requests</a>
             <a href="#gallery">Gallery</a>
             <a href="#services">Services</a>
             <a href="#contact">Contact Us</a>
+          </nav> */}
+
+          <nav className={`nav ${open ? "show" : ""}`}>
+            <a href="#about" onClick={() => setOpen(false)}>
+              About Us
+            </a>
+            <a href="#events" onClick={() => setOpen(false)}>
+              Events
+            </a>
+            <a href="#prayer" onClick={() => setOpen(false)}>
+              Prayer Requests
+            </a>
+            <a href="#gallery" onClick={() => setOpen(false)}>
+              Gallery
+            </a>
+            <a href="#services" onClick={() => setOpen(false)}>
+              Services
+            </a>
+            <a href="#contact" onClick={() => setOpen(false)}>
+              Contact Us
+            </a>
           </nav>
 
           {/* Button */}
           <div className="cta">
             <button className="start-btn">Login</button>
+          </div>
+          {/* Hamburger Menu for Mobile */}
+          <div className="hamburger" onClick={() => setOpen(prev => !prev)}>
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
         </div>
       </header>
